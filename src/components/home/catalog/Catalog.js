ac7c3a5750d49c1814336eb75f096d15a5051f21
jesuanp <jespaterm@gmail.com>
2023-02-13 14:34:59 -0400

    haciendo que se le pueda hacer scroll horizontal a los proyectos

--- src/components/home/catalog/Catalog.js
+++ src/components/home/catalog/Catalog.js
@@ -4,32 +4,67 @@ import image2 from '../../../images/image-robot-2.jpeg';
 import image3 from '../../../images/image-robot-3.jpeg';
 import image4 from '../../../images/image-robot-4.jpeg';
 import image5 from '../../../images/image-robot-5.jpeg';
+import image6 from '../../../images/image-robot-6.jpeg';
+import image7 from '../../../images/image-robot-7.jpeg';
 
 
 export default function Catalog() {
 
     return (
-        <div className={s.container}>
-
-            <h2 style={{textAlign: 'center'}}>CATALOG</h2>
-
-            <div className={s.catalog}>
-                <div className={s.divImg}>
-                    <img className={s.img} src={image2} alt='imagen del proyecto' />
-                </div>
-
-                <div className={s.divImg}>
-                    <img className={s.img} src={image3} alt='imagen del proyecto' />
-                </div>
-
-                <div className={s.divImg}>
-                    <img className={s.img} src={image4} alt='imagen del proyecto' />
-                </div>
-
-                <div className={s.divImg}>
-                    <img className={s.img} src={image5} alt='imagen del proyecto' />
+        <>
+            <div className={s.container}>
+                <div className={s.carrousel}>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image2} alt='imagen del proyecto' />
+                    </div>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image3} alt='imagen del proyecto' />
+                    </div>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image4} alt='imagen del proyecto' />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div className={s.container}>
+                <div className={s.carrousel}>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image5} alt='imagen del proyecto' />
+                    </div>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image6} alt='imagen del proyecto' />
+                    </div>
+                    <div className={s.divImg}>
+                        <img className={s.img} src={image7} alt='imagen del proyecto' />
+                    </div>
+                </div>
+            </div>
+        </>
     )
+
+
+    // return (
+    //     <div className={s.container}>
+
+    //         <h2 style={{textAlign: 'center'}}>CATALOG</h2>
+
+    //         <div className={s.catalog}>
+    //             <div className={s.divImg}>
+    //                 <img className={s.img} src={image2} alt='imagen del proyecto' />
+    //             </div>
+
+    //             <div className={s.divImg}>
+    //                 <img className={s.img} src={image3} alt='imagen del proyecto' />
+    //             </div>
+
+    //             <div className={s.divImg}>
+    //                 <img className={s.img} src={image4} alt='imagen del proyecto' />
+    //             </div>
+
+    //             <div className={s.divImg}>
+    //                 <img className={s.img} src={image5} alt='imagen del proyecto' />
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
 }
